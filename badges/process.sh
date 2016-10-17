af77ebd3eca07d94d41ff2788793e14ce9fc1352
Tom J Nowell <contact@tomjn.com>
2016-10-17 15:39:26 +0100

    fix some issues with a typo and sed quotes

--- badges/process.sh
+++ badges/process.sh
@@ -13,23 +13,24 @@ do
 		cp template.svg temp.svg
 
 		# search replace name1
-		sed -i -e 's/name1/$name/g' temp.svg
+		sed -i -e "s/name1/$name/g" temp.svg
 
 		# search replace twitter1
-		sed -i -e 's/twitter1/$twitter/g' temp.svg
+		sed -i -e "s/twitter1/$twitter/g" temp.svg
 		# search replace name2
-		sed -i -e 's/name2/$othername/g' temp.svg
+		sed -i -e "s/name2/$othername/g" temp.svg
 		# search replace twitter2
-		sed -i -e 's/twitter2/$othertwitter/g' temp.svg
+		sed -i -e "s/twitter2/$othertwitter/g" temp.svg
 
 		# output PDF
-		svg-convert -f pdf -o next.pdf temp.svg
+		rsvg-convert -f pdf -o next.pdf temp.svg
 
 		# remove temp.svg
-		rm temp.svg
+		#rm temp.svg
+		exit;
 
 		# append page to master PDF
-		gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=finished.pdf next.pdf
+		#gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=finished.pdf next.pdf
 	else
 		othername=$name;
 		othertwitter=$twitter;
