89110e490a5efef7b6c7c2294e44756ce078b9ce
Tom J Nowell <contact@tomjn.com>
2016-10-19 11:36:09 +0100

    switch process.sh to use the one template for all badges and handle the badge type too

--- badges/process.sh
+++ badges/process.sh
@@ -5,21 +5,22 @@ finalcount=0;
 count=0;
 othername='';
 othertwitter='';
+otherrole='';
 rm -r pdfs;
 mkdir pdfs;
-while IFS=, read ticket name twitter first last
+while IFS=, read role name twitter first last
 do
 	let count=count+1;
 	let finalcount=finalcount+1;
 	  if [ $count -eq 2 ]
 	  then
 		count=0;
-	    echo "Handling: ${name} ${twitter}";
-	    echo "Handling2: ${othername} ${othertwitter}";
-		cp template.svg temp.svg
+	    echo "Handling: ${role} ${name} ${twitter}";
+	    echo "Handling2: ${otherrole} ${othername} ${othertwitter}";
+		cp badge.svg temp.svg
 
 		# search replace names and handles
-		sed -i -e "s/name1/$name/g;s/twitter1/$twitter/g;s/name2/$othername/g;s/twitter2/$othertwitter/g" temp.svg
+		sed -i -e "s/name1/$name/g;s/twitter1/$twitter/g;s/name2/$othername/g;s/twitter2/$othertwitter/g;s/type1/$role/g;s/type2/$otherrole/g" temp.svg
 
 		# output PDF
 		#rsvg-convert -f pdf -o pdfs/$finalcount.pdf temp.svg
@@ -32,6 +33,7 @@ do
 	else
 		othername=$name;
 		othertwitter=$twitter;
+		otherrole=$role;
 	fi;
 done < "names.csv"
 
